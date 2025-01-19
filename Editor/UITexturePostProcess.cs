faee8f3205ffdd5e9d08d946bad21b8fcce1530a
Cat <cat.ursu30@gmail.com>
2025-01-19 18:54:53 +0300

    Added textures postProcess

---
+++ Editor/UITexturePostProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Helpers.Editor
+{
+public class UISpritePostProcess : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        var name = Path.GetFileName(assetPath);
+
+        if (name.StartsWith("T_ui_"))
+            PreprocessUISprites(name);
+    }
+
+    void PreprocessUISprites(string name)
+    {
+        var importer = (TextureImporter) assetImporter;
+            
+        if (importer.textureType == TextureImporterType.Sprite)
+            return;
+            
+        Debug.Log($"[UISpritePostProcess] Processing UI sprite - {name} ");
+        importer.textureType = TextureImporterType.Sprite;
+        importer.mipmapEnabled = false;
+        importer.spriteImportMode = SpriteImportMode.Single;
+    }
+}
+}
