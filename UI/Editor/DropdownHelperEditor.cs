e1ee726deeeb713360c4840a2f772609dc9550a2
Cat <cat.ursu30@gmail.com>
2025-01-27 18:35:36 +0300

    Added propertyDrawer for DropdownOption and CustomEditor for DropdownHelper

---
+++ UI/Editor/DropdownHelperEditor.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace Helpers.UI.Editor
+{
+[CustomEditor(typeof(DropdownHelper))]
+public class DropdownHelperEditor : SelectableEditor
+{
+#region Fields
+
+    SerializedProperty _caption;
+    SerializedProperty _panel;
+    SerializedProperty _itemParent;
+    SerializedProperty _itemTemplate;
+
+    SerializedProperty _multiSelect;
+    SerializedProperty _addNoneItem;
+    SerializedProperty _addEverythingItem;
+
+    SerializedProperty _animation;
+    SerializedProperty _panelAnimSpeed;
+
+    SerializedProperty _nothingOption;
+    SerializedProperty _mixedOption;
+    SerializedProperty _everythingOption;
+    SerializedProperty _options;
+
+    [NonSerialized] GUIStyle _boxStyle;
+
+    GUIStyle BoxStyle
+    {
+        get
+        {
+            if (_boxStyle == null)
+            {
+                _boxStyle = new GUIStyle(GUI.skin.window);
+                _boxStyle.padding = new RectOffset(2, 2, 5, 5);
+                _boxStyle.alignment = TextAnchor.UpperCenter;
+            }
+
+            return _boxStyle;
+        }
+    }
+
+#endregion
+
+#region Monobeh
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        _caption = serializedObject.FindProperty("Caption");
+        _panel = serializedObject.FindProperty("Panel");
+        _itemTemplate = serializedObject.FindProperty("ItemTemplate");
+        _itemParent = serializedObject.FindProperty("ItemParent");
+
+        _multiSelect = serializedObject.FindProperty("MultiSelect");
+        _addNoneItem = serializedObject.FindProperty("AddNoneItem");
+        _addEverythingItem = serializedObject.FindProperty("AddEverythingItem");
+
+        _animation = serializedObject.FindProperty("AnimationType");
+        _panelAnimSpeed = serializedObject.FindProperty("PanelAnimSpeed");
+
+        _nothingOption = serializedObject.FindProperty("NothingOption");
+        _mixedOption = serializedObject.FindProperty("MixedOption");
+        _everythingOption = serializedObject.FindProperty("EverythingOption");
+        _options = serializedObject.FindProperty("Options");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        DrawElements();
+        DrawAnimation();
+        DrawOptions();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+#endregion
+
+#region Private methods
+
+    void DrawElements()
+    {
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Elements", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical(BoxStyle);
+
+        EditorGUILayout.PropertyField(_caption);
+        EditorGUILayout.PropertyField(_panel);
+        EditorGUILayout.PropertyField(_itemTemplate);
+        EditorGUILayout.PropertyField(_itemParent);
+
+        EditorGUILayout.EndVertical();
+    }
+
+    void DrawAnimation()
+    {
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Animation", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical(BoxStyle);
+
+        EditorGUILayout.PropertyField(_animation);
+        if (_animation.enumValueIndex != 0)
+            EditorGUILayout.PropertyField(_panelAnimSpeed);
+
+        EditorGUILayout.EndVertical();
+    }
+
+    void DrawOptions()
+    {
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Options", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical(BoxStyle);
+
+        EditorGUILayout.PropertyField(_multiSelect);
+        if (_multiSelect.boolValue)
+        {
+            // Show additional options (None, Mixed, Everything
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginVertical(BoxStyle);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_mixedOption);
+
+            EditorGUILayout.Space();
+            EditorGUI.DrawRect(EditorGUILayout.GetControlRect(false, 1), Color.gray);
+            EditorGUILayout.PropertyField(_addNoneItem);
+            if (_addNoneItem.boolValue)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(_nothingOption);
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUILayout.Space();
+            EditorGUI.DrawRect(EditorGUILayout.GetControlRect(false, 1), Color.gray);
+            EditorGUILayout.PropertyField(_addEverythingItem);
+            if (_addEverythingItem.boolValue)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(_everythingOption);
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+        }
+        else
+        {
+            EditorGUI.indentLevel++;
+            EditorGUI.indentLevel--;
+            
+            _addNoneItem.boolValue = false;
+            _addEverythingItem.boolValue = false;
+        }
+
+        EditorGUILayout.Space();
+        EditorGUI.indentLevel++;
+        EditorGUILayout.PropertyField(_options);
+
+        EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical();
+    }
+
+#endregion
+}
+}
---
+++ UI/Editor/DropdownOptionDrawer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UI.Editor
+{
+[CustomPropertyDrawer(typeof(DropdownOption))]
+public class DropdownOptionDrawer : PropertyDrawer
+{
+#region Monobeh
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => EditorGUIUtility.singleLineHeight * 5;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        position = EditorGUI.PrefixLabel(position, label);
+        position.height = EditorGUIUtility.singleLineHeight;
+        position.width += EditorGUIUtility.labelWidth;
+        position.x -= EditorGUIUtility.labelWidth;
+        position.y += EditorGUIUtility.singleLineHeight;
+        EditorGUI.indentLevel++;
+
+        var useLocalizedTextProp = DrawLocalizedBoolCheck(ref position, property);
+        DrawTextField(ref position, property, useLocalizedTextProp);
+        DrawSpriteField(ref position, property);
+
+        EditorGUI.indentLevel--;
+        EditorGUI.EndProperty();
+    }
+
+#endregion
+
+#region Private Methods
+
+    static SerializedProperty DrawLocalizedBoolCheck(ref Rect position, SerializedProperty property)
+    {
+        SerializedProperty useLocalizedTextProp = property.FindPropertyRelative("_useLocalizedText");
+        EditorGUI.PropertyField(position, useLocalizedTextProp, new GUIContent("Localized Text"));
+        position.y += EditorGUIUtility.singleLineHeight;
+        return useLocalizedTextProp;
+    }
+
+    static void DrawTextField(ref Rect position, SerializedProperty property, SerializedProperty useLocalizedTextProp)
+    {
+        if (useLocalizedTextProp.boolValue)
+        {
+            SerializedProperty localizedTxtProp = property.FindPropertyRelative("_localizedTxt");
+            EditorGUI.PropertyField(position, localizedTxtProp, new GUIContent("Text"));
+        }
+        else
+        {
+            SerializedProperty usualTxtProp = property.FindPropertyRelative("_usualTxt");
+            EditorGUI.PropertyField(position, usualTxtProp, new GUIContent("Text"));
+        }
+
+        position.y += EditorGUIUtility.singleLineHeight;
+    }
+
+    static void DrawSpriteField(ref Rect position, SerializedProperty property)
+    {
+        SerializedProperty spriteProp = property.FindPropertyRelative("_sprite");
+        EditorGUI.PropertyField(position, spriteProp, new GUIContent("Image"));
+    }
+
+#endregion
+}
+}
