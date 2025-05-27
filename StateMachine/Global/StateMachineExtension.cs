6a3892c1f2af1cb14ebd0d4e6589c527ad8b2e80
Cat <cat.ursu30@gmail.com>
2025-05-27 11:04:30 +0300

    StateMachine: Added Extension to get scene from SceneLoadResult array

--- StateMachine/Global/StateMachineExtension.cs
+++ StateMachine/Global/StateMachineExtension.cs
@@ -9,7 +9,14 @@ public static class StateMachineExtension
     /// Find and return <b>ISceneEntry</b> component in FirstRootObject's children. RootObject's name must be "[Setup]". 
     /// </summary>
     public static IEntryPoint FindEntryPoint(this Scene scene) =>
-        (from rootObject in scene.GetRootGameObjects() where rootObject.name.Equals("[Setup]")
+        (from rootObject in scene.GetRootGameObjects()
+         where rootObject.name.ToLower().Equals("[setup]")
          select rootObject.GetComponentInChildren<IEntryPoint>()).FirstOrDefault();
+
+    /// <summary>
+    /// Return scene by name from <see cref="SceneLoadResult"/> array.
+    /// </summary>
+    public static Scene GetScene(this SceneLoadResult[] sceneLoadResults, string name) => 
+        sceneLoadResults.FirstOrDefault(result => result.LoadedScene.name.Equals(name))!.LoadedScene;
 }
 }
