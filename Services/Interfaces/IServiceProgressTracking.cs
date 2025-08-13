249c3edb1114a08d54b87989b3a2900db1fea236
Cat <cat.ursu30@gmail.com>
2025-08-13 18:11:10 +0300

    Service: Moved ProgressTrackingService, update it documentation, updated UnloadsTracking logic

---
+++ Services/Interfaces/IServiceProgressTracking.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace Helpers.Services
+{
+public interface IServiceProgressTracking
+{
+    int LoadProgressCount { get; set; }
+    bool UnloadsAreFinished { get; }
+    event Action<float> OnUpdateProgress;
+    event Action<string> OnUpdateLoadingTip;
+
+    /// <summary>
+    /// Reset Loading and Unloading processes. <br/>
+    /// <b>IMPORTANT:</b> Call before show LoadingScreen or whenever need to reset progress (f.e. after hide LoadingScreen).
+    /// </summary>
+    void ResetProgress();
+    
+    /// <summary>
+    /// Store <b>loadings</b> at runtime to have ref to original value during whole loading
+    /// Store LoadingProgress to update loading bar. Should be called in <b>StateEnter</b> of each scene
+    /// </summary>
+    void RegisterLoadingProgress(SceneLoadProgress sceneLoadProgress);
+
+    /// <summary>
+    /// Register unload tasks and start track till all tasks are complete<br/>
+    /// To check if unloads are finished, use <b><see cref="UnloadsAreFinished"/></b> field. 
+    /// </summary>
+    void RegisterUnloadProcesses(params UniTask[] unloadingTasks);
+    
+    /// <summary>
+    /// Update progress of loading and unloading tasks and give the Avg of them <br/>
+    /// </summary>
+    void UpdateProgress();
+    
+    /// <summary>
+    /// Update the tip on loading screen. (f.e. "Loading level 1", "Loading assets", etc.)
+    /// </summary>
+    void UpdateLoadingTip(string progressTip);
+}
+}
---
+++ Services/ProgressTrackingService.cs
@@ -0,0 +1,85 @@
+﻿using R3;
+using System;
+using System.Linq;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Helpers.Services
+{
+public class ProgressTrackingService : IServiceProgressTracking
+{
+#region Fields
+
+    public int LoadProgressCount { get; set; }
+    public bool UnloadsAreFinished { get; private set; }
+    public event Action<float> OnUpdateProgress;
+    public event Action<string> OnUpdateLoadingTip;
+
+    readonly List<SceneLoadProgress> _loadingProcesses = new();
+    readonly List<UniTask> _unloadingTasks = new();
+    readonly CompositeDisposable _disposable = new();
+    readonly TimeSpan _delayTimeSpan = TimeSpan.FromSeconds(.25f);
+
+#endregion
+
+#region Public methods
+
+    public ProgressTrackingService() => ResetProgress();
+
+    public void ResetProgress()
+    {
+        _loadingProcesses.Clear();
+        _unloadingTasks.Clear();
+        LoadProgressCount = 0;
+        UnloadsAreFinished = true;
+
+        OnUpdateProgress?.Invoke(0);
+        OnUpdateLoadingTip?.Invoke(string.Empty);
+    }
+    
+    public void RegisterLoadingProgress(SceneLoadProgress sceneLoadProgress) => _loadingProcesses.Add(sceneLoadProgress);
+
+    public void RegisterUnloadProcesses(params UniTask[] unloadingTasks)
+    {
+        if (unloadingTasks == null || unloadingTasks.Length == 0)
+        {
+            Debug.LogWarning("No unloading tasks provided.");
+            return;
+        }
+
+        foreach (var unloadTask in unloadingTasks)
+            _unloadingTasks.Add(unloadTask);
+
+        UnloadsAreFinished = false;
+        _disposable.Clear();
+        Observable.Interval(_delayTimeSpan)
+                  .TakeUntil(_ => UnloadsAreFinished)
+                  .Subscribe(_ => UpdateProgress())
+                  .AddTo(_disposable);
+    }
+
+    public void UpdateProgress()
+    {
+        var unloadProgress = _unloadingTasks.Count != 0 ? GetUnloadTasksProgress() : 1;
+        var loadProgress = LoadProgressCount != 0 ? GetLoadTasksProgress() : 0;
+        OnUpdateProgress?.Invoke((loadProgress + unloadProgress) / 2);
+        return;
+
+        float GetLoadTasksProgress() => _loadingProcesses.Sum(progress => progress.GetAvgProgress()) / LoadProgressCount;
+
+        float GetUnloadTasksProgress()
+        {
+            var progress = _unloadingTasks.Select(task => task.GetAwaiter().IsCompleted ? 1 : 0)
+                                                .Average(progress => (float)progress);
+            UnloadsAreFinished = Mathf.Approximately(progress, 1);
+
+            return progress;
+        }
+    }
+
+    public void UpdateLoadingTip(string progressTip) => OnUpdateLoadingTip?.Invoke(progressTip);
+
+#endregion
+}
+}
